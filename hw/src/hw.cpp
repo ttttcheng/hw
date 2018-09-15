ef36c6d54807a0fa8c8bd35c5afcee8bf353845c
ttttcheng <ttttcheng@192.168.1.2>
2018-09-15 14:48:00 +0800

    first commit

---
+++ hw/src/hw.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : hw.cpp
+// Author      : tony
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	double number1;
+	double number2;
+
+	double sum;
+	cout << "Enter the first number" << endl; // endl makes a new line
+	cin >> number1;
+	cout << "Enter the second number" << endl; // endl makes a new line
+	cin >> number2;
+	// added together
+	sum = number1 + number2;
+	cout << "loading..." << endl;
+	cout << sum;
+	return 0;
+}
